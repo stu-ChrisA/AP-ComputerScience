d2d8722596e4d549370b0ee1f887548642c73fbb
stu-ChrisA <92584525+stu-ChrisA@users.noreply.github.com>
2021-10-19 08:58:03 -0400

    Update Hailstone1.java

--- Hailstone1.java
+++ Hailstone1.java
@@ -7,22 +7,19 @@ public class HailStone1
         System.out.println(start);
         for(int i = 1; i < 200; i++)
         {
-    
-            while(i < 200)
+            if(start%2 == 0)
             {
-                if(start%2 == 0)
-                {
-                    start = start/2;
-                    steps++;
-                }
-                else if(start%2 != 0)
-                {
-                    start = start * 3 + 1;
-                    steps++;
-                }
-                System.out.print(start);
+                start = start/2;
+                steps++;
             }
+            else if(start%2 != 0)
+            {
+                start = start * 3 + 1;
+                steps++;
+            }
+            System.out.print(start + " ");
         }
+        
             System.out.println("Steps: " + steps + " Took this amount of steps");
         //a - Yes all intergers eventually reach this cycle
         //b - The maxium is the amount it's starting with until it hits the number 1
